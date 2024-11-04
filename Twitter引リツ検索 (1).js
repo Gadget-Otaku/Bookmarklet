4e100c84bab9a80b251aaacb6271e4ff8c8ce747
Gadget-Otaku <youhaosen2@gmail.com>
2024-11-03 19:04:01 -0600

    Delete Twitter引リツ検索 (1).js

--- Twitter引リツ検索 (1).js
+++
@@ -1,4 +0,0 @@
-export default () => {
-    const title = document.title;
-    const url = document.URL;
-    navigator.clipboard.javascript:(function() {    var url = window.location.href;    if (!url.endsWith('/quotes')) {        window.location.href = url + '/quotes';    }})();(`[${title}](${url})`).then();
