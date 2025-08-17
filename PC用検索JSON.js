36ec790d21c8249660024f4ad4ee846e1b506b9f
Gadget-Otaku <youhaosen2@gmail.com>
2025-08-17 14:44:10 -0500

    Automatic update from PC 2025/08/17 14:44:10.96

---
+++ PC用検索JSON.js
@@ -0,0 +1,32 @@
+[
+  {
+    "name": "Google",
+    "iconUrl": "https://www.google.com/s2/favicons?domain=google.com&sz=64",
+    "urlPrefix": "https://www.google.com/search?q=",
+    "urlSuffix": "",
+    "position": {
+      "top": 27,
+      "left": 16
+    },
+    "size": 40,
+    "borderRadius": 20
+  },
+  {
+    "name": "DuckDuckGo",
+    "iconUrl": "https://www.google.com/s2/favicons?domain=duckduckgo.com&sz=64",
+    "urlPrefix": "https://duckduckgo.com/?q=",
+    "urlSuffix": ""
+  },
+  {
+    "name": "Github ファイル名からリポジトリ ",
+    "iconUrl": "https://www.google.com/s2/favicons?domain=github.com&sz=64",
+    "urlPrefix": "https://github.com/Gadget-Otaku/Bookmarklet/blob/main/",
+    "urlSuffix": ""
+  },
+  {
+    "name": "Astate",
+    "iconUrl": "https://e7.pngegg.com/pngimages/434/498/png-clipart-arkansas-state-red-wolves-football-arkansas-state-red-wolves-men-s-basketball-arkansas-state-red-wolves-women-s-basketball-camellia-bowl-boar-animals-carnivoran.png",
+    "urlPrefix": "https://www.astate.edu/search/?q=",
+    "urlSuffix": ""
+  }
+]
---
+++ スマホ用検索JSON.js
@@ -0,0 +1,50 @@
+[
+  {
+    "name": "Google",
+    "iconUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYS_ocSphOFHVuKC9WgglMIvN44YC0op4uJ5rZ-qjcVhMJmwbYnudOngqiHLMTgb_kfr0&usqp=CAU",
+    "urlPrefix": "https://www.google.com/search?q=",
+    "urlSuffix": "",
+    "position": {
+      "top": 15,
+      "left": 15
+    },
+    "size": 40,
+    "borderRadius": 20
+  },
+  {
+    "name": "DuckDuckGo",
+    "iconUrl": "https://www.google.com/s2/favicons?domain=duckduckgo.com&sz=64",
+    "urlPrefix": "https://duckduckgo.com/?q=",
+    "urlSuffix": ""
+  },
+  {
+    "name": "原神",
+    "iconUrl": "https://upload-os-bbs.hoyolab.com/upload/2021/08/05/72354102/caca7eebb8162aaebc7c8b729734242b_2822963320735910337.jpg",
+    "urlPrefix": "https://www.google.com/search?q=site%3Agamewith.jp%20OR%20site%3Awikiwiki.jp%20",
+    "urlSuffix": ""
+  },
+  {
+    "name": "原神wiki",
+    "iconUrl": "https://www.google.com/s2/favicons?domain=wikiwiki.jp&sz=64",
+    "urlPrefix": "https://www.google.com/search?q=site%3Awikiwiki.jp%20",
+    "urlSuffix": ""
+  },
+  {
+    "name": "ヒトミラ",
+    "iconUrl": "https://www.google.com/s2/favicons?domain=hitomi.la&sz=64",
+    "urlPrefix": "https://www.google.com/search?q=site%3Ahitomi.la%20",
+    "urlSuffix": ""
+  },
+  {
+    "name": "モモンガ",
+    "iconUrl": "https://www.google.com/s2/favicons?domain=momon-ga.com&sz=64",
+    "urlPrefix": "https://www.google.com/search?q=site%3Amomon-ga.com%20",
+    "urlSuffix": ""
+  },
+  {
+    "name": "MissAV",
+    "iconUrl": "https://www.google.com/s2/favicons?domain=missav.ai&sz=64",
+    "urlPrefix": "https://www.google.com/search?q=site%3Amissav.ai%20",
+    "urlSuffix": ""
+  }
+]
